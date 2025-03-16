89e1c2081fd30e994d57255e32205b1811373076
vkz. <lucaspedgley1@gmail.com>
2025-03-16 23:44:08 +0300

    Update whitelist.lua

--- mm2/whitelist.lua
+++ mm2/whitelist.lua
@@ -1,19 +1,4 @@
-local Players = game:GetService("Players")
-
-whitelisted = {
-    "uz0ci",
-    "vkzire",
-    "arabkeng"
+return {
+8107931136,
+2914243596
 }
-
-local checked = 0
-local ids = {}
-for i, v in pairs(whitelisted) do
-    task.spawn(function()
-        table.insert(ids, Players:GetUserIdFromNameAsync(v))
-        checked +=1
-    end)
-end
-
-repeat wait() until checked == #whitelisted
-return ids
